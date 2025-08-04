e6ff3895980de8f001aca26bf63b139e7810c35a
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-04 17:35:18 +0100

    Chore: Move localStorage functions to seperate module and import to index.js

---
+++ src/utils/localStorage.js
@@ -0,0 +1,11 @@
+
+const shoppingListKey = "shoppingListItems";
+
+export function getItems() {
+  const items = localStorage.getItem(shoppingListKey);
+  return items ? JSON.parse(items) : [];
+}
+
+export function saveItems(items) {
+  localStorage.setItem(shoppingListKey, JSON.stringify(items));
+}
