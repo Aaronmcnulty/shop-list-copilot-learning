6dc722b12da90d4b06c2d40b507d854873dafa5c
aaronmcnulty <malcomcrabesq@outlook.com>
2025-08-07 14:08:02 +0100

    Tests: move repetitive function calls and spyOn imports to beforeEach function. Add function call checks to tests that return error messages.

--- src/modules/handleAddItemClick/handleAddItemClick.test.js
+++ src/modules/handleAddItemClick/handleAddItemClick.test.js
@@ -1,20 +1,30 @@
-import { handleAddItemClick } from "./handleAddItemClick";
-import { renderList } from "../renderList/renderList.js";
+import { handleAddItemClick } from "./handleAddItemClick.js";
 import { getItems, saveItems } from "../localStorage/localStorage.js";
 
 describe("handleAddItemClick", () => {
+  
+  let saveItemsSpy, getItemsSpy, renderListSpy;
+
   beforeEach(() => {
-    // Create mock item array to be used in tests.
-    let itemsArray = [
+
+    // Setup DOM and localStorage
+    document.body.innerHTML = '<div id="shopping-list"></div>';
+    const itemsArray = [
       { name: "Apples", quantity: 2 },
       { name: "Oranges", quantity: 2 },
     ];
 
-    // Render element for the shopping list.
-    document.body.innerHTML = '<div id="shopping-list"></div>';
-
-    // Store the mock items array in localStorage.
     saveItems("shoppingListItems", itemsArray);
+
+    // Setup spies
+    saveItemsSpy = jest.spyOn(require("../localStorage/localStorage.js"), "saveItems");
+    getItemsSpy = jest.spyOn(require("../localStorage/localStorage.js"), "getItems");
+    renderListSpy = jest.spyOn(require("../renderList/renderList.js"), "renderList");
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    localStorage.clear();
   });
 
   test("Should call renderList and pass getItems and saveItems as arguements.", () => {
@@ -22,17 +32,11 @@ describe("handleAddItemClick", () => {
     const nameValue = "Banana";
     const quantityValue = 3;
 
-    // Spy on the renderList function
-    const renderList = jest.spyOn(
-      require("../renderList/renderList.js"),
-      "renderList",
-    );
-
     // Call the function with mocked values.
     handleAddItemClick(nameValue, quantityValue);
 
     // Check if renderList was called with the correct arguments.
-    expect(renderList).toHaveBeenCalledWith(getItems, saveItems);
+    expect(renderListSpy).toHaveBeenCalledWith(getItems, saveItems);
   });
 
   test("Should call saveItems and pass the updated array as an argument.", () => {
@@ -40,14 +44,9 @@ describe("handleAddItemClick", () => {
     const nameValue = "Banana";
     const quantityValue = 3;
 
-    const saveItems = jest.spyOn(
-      require("../localStorage/localStorage.js"),
-      "saveItems",
-    );
-
     // Call the function
     handleAddItemClick(nameValue, quantityValue);
-    expect(saveItems).toHaveBeenCalledWith("shoppingListItems", [
+    expect(saveItemsSpy).toHaveBeenCalledWith("shoppingListItems", [
       { name: "Apples", quantity: 2 },
       { name: "Oranges", quantity: 2 },
       { name: "Banana", quantity: 3 },
@@ -59,24 +58,23 @@ describe("handleAddItemClick", () => {
     const nameValue = "Banana";
     const quantityValue = 3;
 
-    // Spy on the getItems function
-    const getItems = jest.spyOn(
-      require("../localStorage/localStorage.js"),
-      "getItems",
-    );
 
     // Call the function with mocked values.
     handleAddItemClick(nameValue, quantityValue);
     // Check if getItems was called with the correct key.
-    expect(getItems).toHaveBeenCalledWith("shoppingListItems");
+    expect(getItemsSpy).toHaveBeenCalledWith("shoppingListItems");
   });
 
-  test("Should not be throw error when quantityValue type is string.", () => {
+  test("Should throw error when quantityValue type is string.", () => {
     // Emepty string mock input values
     const nameValue = "Grapes";
     const quantityValue = "";
 
     // Check that getItems, saveItems and renderList were not called.
+    expect(saveItemsSpy).not.toHaveBeenCalled();
+    expect(getItemsSpy).not.toHaveBeenCalled();
+    expect(renderListSpy).not.toHaveBeenCalled();
+    // Expect an error to be thrown.
     expect(() => {handleAddItemClick(nameValue, quantityValue)}).toThrow("Invalid input: nameValue must be a string and quantityValue must be a number.");
   });
 
@@ -85,7 +83,12 @@ describe("handleAddItemClick", () => {
     const nameValue = "Grapes";
     const quantityValue = 0;
 
+
     // Check that getItems, saveItems and renderList were not called.
+    expect(saveItemsSpy).not.toHaveBeenCalled();
+    expect(getItemsSpy).not.toHaveBeenCalled();
+    expect(renderListSpy).not.toHaveBeenCalled();
+    // Expect an error to be thrown.
     expect(() => {handleAddItemClick(nameValue, quantityValue)}).toThrow("Invalid input: Name cannot be empty and quantity must be at least 1.");
   
   });
@@ -96,6 +99,10 @@ describe("handleAddItemClick", () => {
     const quantityValue = -2;
 
     // Check that getItems, saveItems and renderList were not called.
+    expect(saveItemsSpy).not.toHaveBeenCalled();
+    expect(getItemsSpy).not.toHaveBeenCalled();
+    expect(renderListSpy).not.toHaveBeenCalled();
+    // Expect an error to be thrown.
     expect(() => {handleAddItemClick(nameValue, quantityValue)}).toThrow("Invalid input: Name cannot be empty and quantity must be at least 1.");
 
   });
@@ -105,9 +112,11 @@ describe("handleAddItemClick", () => {
     const nameValue = undefined;
     const quantityValue = undefined;
 
-    
-
    // Check that getItems, saveItems and renderList were not called.
+   expect(saveItemsSpy).not.toHaveBeenCalled();
+    expect(getItemsSpy).not.toHaveBeenCalled();
+    expect(renderListSpy).not.toHaveBeenCalled();
+    // Expect an error to be thrown.
     expect(() => {handleAddItemClick(nameValue, quantityValue)}).toThrow("Invalid input: nameValue must be a string and quantityValue must be a number.");
   
   });
@@ -116,7 +125,11 @@ describe("handleAddItemClick", () => {
     const nameValue = null;
     const quantityValue = null;
 
-
+    // Check that getItems, saveItems and renderList were not called.
+    expect(saveItemsSpy).not.toHaveBeenCalled();
+    expect(getItemsSpy).not.toHaveBeenCalled();
+    expect(renderListSpy).not.toHaveBeenCalled();
+    // Expect an error to be thrown.
     expect(() => {handleAddItemClick(nameValue, quantityValue)}).toThrow("Invalid input: nameValue must be a string and quantityValue must be a number.");
   });
 
@@ -124,6 +137,11 @@ describe("handleAddItemClick", () => {
     const nameValue = NaN;
     const quantityValue = NaN;
 
+    // Check that getItems, saveItems and renderList were not called.
+    expect(saveItemsSpy).not.toHaveBeenCalled();
+    expect(getItemsSpy).not.toHaveBeenCalled();
+    expect(renderListSpy).not.toHaveBeenCalled();
+    // Expect an error to be thrown.
     expect(() => {handleAddItemClick(nameValue, quantityValue)}).toThrow("Invalid input: nameValue must be a string and quantityValue must be a number.");
 
   });
